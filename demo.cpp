ead59b7ca0bc947765e20d6a04571f1e99a5732d
cedelgadosalazar <cedelgadosalazar@gmail.com>
2021-05-25 18:22:43 -0500

    Ejemplo hecho en clase

---
+++ demo.cpp
@@ -0,0 +1,23 @@
+#include "demo.h"
+#include "ui_demo.h"
+
+Demo::Demo(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Demo)
+{
+    ui->setupUi(this);
+    connect(ui->btn_Exit, &QAbstractButton::clicked, this, &QWidget::close);
+}
+
+Demo::~Demo()
+{
+    delete ui;
+}
+
+
+void Demo::on_btn_Close_clicked()
+{
+    qInfo() << "Click Button";
+    close();
+}
+
---
+++ demo.h
@@ -0,0 +1,25 @@
+#ifndef DEMO_H
+#define DEMO_H
+
+#include <QWidget>
+#include <QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Demo; }
+QT_END_NAMESPACE
+
+class Demo : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Demo(QWidget *parent = nullptr);
+    ~Demo();
+
+private slots:
+    void on_btn_Close_clicked();
+
+private:
+    Ui::Demo *ui;
+};
+#endif // DEMO_H
